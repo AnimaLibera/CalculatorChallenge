d5fd61b9cf644c794a9d12f9b2335081efe33982
Gianni-Lauritz Grubert <animalibera@mail.de>
2024-04-30 20:56:29 +0200

    Basic valid Checks added

--- main.py
+++ main.py
@@ -10,6 +10,29 @@ def test():
     #print(is_float("12..4"))
     #print(is_float("abc"))
 
+    #print(is_parentheses("abc"))
+    #print(is_parentheses("("))
+    #print(is_parentheses(")"))
+    #print(is_parentheses("()"))
+
+    #print(is_valid_symbol("1.2"))
+    #print(is_valid_symbol("1"))
+    #print(is_valid_symbol("("))
+    #print(is_valid_symbol(""))
+    #print(is_valid_symbol("abc"))
+
+    #print(is_multiplication("*"))
+    #print(is_multiplication("!"))
+
+    #print(is_division("/"))
+    #print(is_division("!"))
+
+    #print(is_addition("+"))
+    #print(is_addition("!"))
+
+    #print(is_subtraction("-"))
+    #print(is_subtraction("!"))
+
 def main():
     print("Challenge Calculator")
     
@@ -24,24 +47,69 @@ def filter_for_valid_symbols(notation):
 
     for character in notation:
         temporer_notation += character
-        if is_integer(temporer_notation):
+        if is_valid_symbol(temporer_notation):
             filterd_notation.append(temporer_notation)
             temporer_notation = ""
-        elif is_float(temporer_notation):
-            filterd_notation.append(temporer_notation)
-            temporer_notation = ""
+    
+    if len(temporer_notation) != 0:
+        print(f"Error! Restnoation: {temporer_notation}")
 
-def check_valid_symbol(notation):
-    pass
+def is_valid_symbol(symbol):
+    """Check if symbol (string) is valid operator, operant or parentheses"""
+
+    return is_operant(symbol) or is_operator(symbol) or is_parentheses(symbol)
 
 def is_operator(symbol):
-    pass
+    """Check if symbol (string) is valid operator"""
+
+    return False
 
 def is_operant(symbol):
-    pass
+    """Check if symbol (string) is valid operant"""
+
+    return is_integer(symbol) or is_float(symbol)
 
 def is_parentheses(symbol):
-    pass
+    """Check if symbol (string) is a valid parentheses"""
+
+    if len(symbol) != 1:
+        return False
+    elif symbol not in "()":
+        return False
+
+    return True
+
+def is_multiplication(symbol):
+    """Check if symbol (string) is multiplication operator"""
+    
+    if symbol not in "*":
+        return False
+
+    return True and not is_empty(symbol)
+
+def is_division(symbol):
+    """Check if symbol (string) is division operator"""
+    
+    if symbol not in "/":
+        return False
+
+    return True and not is_empty(symbol)
+
+def is_addition(symbol):
+    """Check if symbol (string) is addition operator"""
+    
+    if symbol not in "+":
+        return False
+
+    return True and not is_empty(symbol)
+
+def is_subtraction(symbol):
+    """Check if symbol (string) is subtraction operator"""
+    
+    if symbol not in "-":
+        return False
+
+    return True and not is_empty(symbol)
 
 def is_integer(symbol):
     """Checks if an symbol (string) is a valid integer"""
@@ -50,7 +118,7 @@ def is_integer(symbol):
         if character not in "0123456789":
             return False
     
-    return True
+    return True and not is_empty(symbol)
 
 def is_float(symbol):
     """Checks if an symbol (string) is a valid float"""
@@ -65,7 +133,15 @@ def is_float(symbol):
                 return False
             dot_detected = True
     
-    return True and dot_detected
+    return True and dot_detected and not is_empty(symbol)
+
+def is_empty(symbol):
+    """Checks if symbol (string) is empty"""
+
+    if len(symbol) != 0:
+        return False
+
+    return True
 
 def transform_to_prefix_notation(infix_notation):
     pass
